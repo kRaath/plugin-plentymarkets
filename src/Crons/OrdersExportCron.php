2190a1fe6b046d02aeb566f46e7e3d880c9685a5
khadim Raath <khadim.raath@gmail.com>
2017-03-15 13:58:29 +0500

    log tets

--- src/Crons/OrdersExportCron.php
+++ src/Crons/OrdersExportCron.php
@@ -4,11 +4,14 @@ namespace EkomiIntegration\Crons;
 
 use Plenty\Modules\Cron\Contracts\CronHandler as Cron;
 use EkomiIntegration\Services\EkomiServices;
+use Plenty\Plugin\Log\Loggable;
 
 /**
  * Class OrdersExportCron
  */
 class OrdersExportCron extends Cron {
+
+    use Loggable;
 
     /**
      *
@@ -22,7 +25,7 @@ class OrdersExportCron extends Cron {
 
     public function handle() {
         $daysDiff = 7;
-        
+
         $this->getLogger(__FUNCTION__)->error('EkomiIntegration::OrdersExportCron.handle', 'CronRunning....');
 
         $this->ekomiServices->sendOrdersData($daysDiff);
