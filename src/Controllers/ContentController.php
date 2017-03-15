cc9f93ad556868390502de1a5591c1586e13f868
khadim Raath <khadim.raath@gmail.com>
2017-03-15 12:42:02 +0500

    test2

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -20,9 +20,15 @@ class ContentController extends Controller {
     //https://developers.plentymarkets.com/rest-doc/order/details#list-orders-by-filter-options
     public function sayHello(Twig $twig, EkomiServices $service, ConfigHelper $helper): string {
 
-        echo $helper->getEnabled();
-        echo '<br/>';
-          $service->sendOrdersData(7);
+        if ($helper->getEnabled()) {
+            echo 'yesss';
+            echo '<br/>';
+        }
+        if ($helper->getEnabled() == 'true') {
+            echo '==true';
+            echo '<br/>';
+        }
+        $service->sendOrdersData(7);
 
         return $twig->render('EkomiIntegration::content.hello');
     }
