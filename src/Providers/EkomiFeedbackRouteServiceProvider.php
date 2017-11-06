a9cf2adc737fc788e75ce5f0da5537549f6e195c
kRaath <khadim.raath@gmail.com>
2017-11-06 17:16:10 +0500

    DB table creation -8

--- src/Providers/EkomiFeedbackRouteServiceProvider.php
+++ src/Providers/EkomiFeedbackRouteServiceProvider.php
@@ -17,7 +17,7 @@ class EkomiFeedbackRouteServiceProvider extends RouteServiceProvider {
     public function map(Router $router) {
         $router->get('sendOrdersToEkomi', 'EkomiFeedback\Controllers\ContentController@sendOrdersToEkomi');
         $router->get('fetchProductReviews', 'EkomiFeedback\Controllers\ContentController@fetchProductReviews');
-        $router->get('reviews', 'EkomiFeedback\Controllers\ContentController@showReview');
+        $router->get('reviews/{pwd}', 'EkomiFeedback\Controllers\ContentController@showReview');
 //        $router->post('reviews', 'EkomiFeedback\Controllers\ContentController@createReview');
 //        $router->put('reviews/{id}', 'EkomiFeedback\Controllers\ContentController@updateReview')->where('id', '\d+');
 //        $router->delete('reviews/{id}', 'EkomiFeedback\Controllers\ContentController@deleteReview')->where('id', '\d+');
