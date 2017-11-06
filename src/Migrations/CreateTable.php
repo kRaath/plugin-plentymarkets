772b5408c6686d0ffc77bd6401078421dbe8b323
kRaath <khadim.raath@gmail.com>
2017-11-06 12:41:02 +0500

    db table deletion

--- src/Migrations/CreateTable.php
+++ src/Migrations/CreateTable.php
@@ -4,18 +4,19 @@ namespace EkomiFeedback\Migrations;
  
 use EkomiFeedback\Models\EkomiFeedbackReviews;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
- 
+use Plenty\Plugin\Log\Loggable;
 /**
  * Class CreateTable
  */
 class CreateTable
 {
+    use Loggable;
     /**
      * @param Migrate $migrate
      */
     public function run(Migrate $migrate)
     {
-        $migrate->deleteTable(EkomiFeedbackReviews::class);
-        $migrate->createTable(EkomiFeedbackReviews::class);
+        $this->getLogger(__FUNCTION__)->error('EkomiFeedback::CreateTable.run', $migrate->deleteTable(EkomiFeedbackReviews::class));
+        $this->getLogger(__FUNCTION__)->error('EkomiFeedback::CreateTable.run', $migrate->createTable(EkomiFeedbackReviews::class));
     }
 }
