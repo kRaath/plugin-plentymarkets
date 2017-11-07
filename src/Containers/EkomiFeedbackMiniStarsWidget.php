090242a6c841bfea8e28cde5df3af99903e2e89c
kRaath <khadim.raath@gmail.com>
2017-11-07 14:33:37 +0500

    ministars -4

--- src/Containers/EkomiFeedbackMiniStarsWidget.php
+++ src/Containers/EkomiFeedbackMiniStarsWidget.php
@@ -1,16 +1,20 @@
 <?php
- 
+
 namespace EkomiFeedback\Containers;
- 
+
 use Plenty\Plugin\Templates\Twig;
- 
-class EkomiFeedbackMiniStarsWidget
-{
-    public function call(Twig $twig):string
-    {
-        return $twig->render('EkomiFeedback::content.miniStarsWidget');
+
+class EkomiFeedbackMiniStarsWidget {
+
+    public function call(Twig $twig): string {
+        $templateData = array("reviewsCount" => 29, "avgRating" => 3.5, 'articleName' => 'Test');
+        
+        
+        return $twig->render('EkomiFeedback::content.miniStarsWidget', $templateData);
     }
+
     public function getAvgRating() {
         return 3.7;
     }
+
 }
