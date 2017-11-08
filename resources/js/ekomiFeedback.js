a7f501b62cbaa13bf62587a1112110281d2b0978
kRaath <khadim.raath@gmail.com>
2017-11-08 15:27:34 +0500

    reviews container

--- resources/js/ekomiFeedback.js
+++ resources/js/ekomiFeedback.js
@@ -1,109 +1,105 @@
-if ( (typeof jQuery === 'undefined') && !window.jQuery ) {
-  // document.write(unescape("%3Cscript type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'%3E%3C/script%3E"));
+if ((typeof jQuery === 'undefined') && !window.jQuery) {
+    // document.write(unescape("%3Cscript type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'%3E%3C/script%3E"));
 } else {
- if((typeof jQuery === 'undefined') && window.jQuery) {
-     jQuery = window.jQuery;
- } else if((typeof jQuery !== 'undefined') && !window.jQuery) {
-     window.jQuery = jQuery;
- }
+    if ((typeof jQuery === 'undefined') && window.jQuery) {
+        jQuery = window.jQuery;
+    } else if ((typeof jQuery !== 'undefined') && !window.jQuery) {
+        window.jQuery = jQuery;
+    }
 }
 
-if(typeof jQuery !== 'undefined'){
+if (typeof jQuery !== 'undefined') {
     jQuery(document).ready(function () {
-    // sorting reviews data
-    jQuery('.ekomi_reviews_sort').on('change', function (e) {
-        filter = this.value;
-        pageOffset = 0;
-        var data = {
-            type: 'loadReviews',
-            articleId: articleId,
-            queryBy: queryBy,
-            pageOffset: pageOffset,
-            reviewsLimit: reviewsLimit,
-            filter: filter
-        };
-
-        jQuery.ajax({
-            type: "POST",
-            url: ajaxUrl,
-            data: data,
-            cache: false,
-            success: function (data) {
-                var json = jQuery.parseJSON(data);
-
-                jQuery('#ekomi_reviews_container').html(json.reviews_data.result);
-
-                // reset the page offset
-
-                pageReviewsCount = json.reviews_data.count;
-
-                jQuery('.current_review_batch').text(pageReviewsCount);
-                jQuery('.loads_more_reviews').show();
-            }
-        });
-    });
-
-    // saving users feedback on reviews
-    jQuery('body').on('click', '.ekomi_review_helpful_button', function () {
-        var current = jQuery(this);
-
-        var data = {
-            type: 'saveFeedback',
-            articleId: articleId,
-            review_id: jQuery(this).data('review-id'),
-            helpfulness: jQuery(this).data('review-helpfulness')
-        };
-
-        jQuery.ajax({
-            type: "POST",
-            url: ajaxUrl,
-            data: data,
-            cache: false,
-            success: function (data) {
-                var json = jQuery.parseJSON(data);
-
-                current.parent('.ekomi_review_helpful_question').hide();
-                current.parent().prev('.ekomi_review_helpful_thankyou').show();
-                var infoMsg= json.helpfull_count+" "+jQuery('.ekomi_prc_out_of').text()+" "+json.total_count+" "+jQuery('.ekomi_prc_people_found').text();
-                current.parent().prev().prev('.ekomi_review_helpful_info').html(infoMsg);
-            }
-        });
-    });
-
-    // Loading reviews on paginatin
-    jQuery('body').on('click', '.loads_more_reviews', function (e) {
-        pageOffset = pageReviewsCount;
-
-        if (reviewsCount / pageReviewsCount > 1) {
+        // sorting reviews data
+        jQuery('.ekomi_reviews_sort').on('change', function (e) {
+            prcFilter = this.value;
+            prcOffset = 0;
             var data = {
-                type: 'loadReviews',
-                articleId: articleId,
+                prcProductId: prcProductId,
                 queryBy: queryBy,
-                pageOffset: pageOffset,
+                prcOffset: prcOffset,
                 reviewsLimit: reviewsLimit,
-                filter: filter
+                prcFilter: prcFilter
             };
 
             jQuery.ajax({
                 type: "POST",
-                url: ajaxUrl,
+                url: ajaxUrl + 'loadReviews',
                 data: data,
                 cache: false,
                 success: function (data) {
                     var json = jQuery.parseJSON(data);
 
-                    pageReviewsCount = pageReviewsCount + json.reviews_data.count;
-                    jQuery('#ekomi_reviews_container').append(json.reviews_data.result);
-                    jQuery('.current_review_batch').text(pageReviewsCount);
+                    jQuery('#ekomi_reviews_container').html(json.result);
 
-                    if (reviewsCount / pageReviewsCount <= 1) {
-                        jQuery('.loads_more_reviews').hide();
-                    }
+                    // reset the page offset
+
+                    reviewsCountPage = json.count;
+
+                    jQuery('.current_review_batch').text(reviewsCountPage);
+                    jQuery('.loads_more_reviews').show();
                 }
             });
-        } else {
-            jQuery('.loads_more_reviews').hide();
-        }
+        });
+
+        // saving users feedback on reviews
+        jQuery('body').on('click', '.ekomi_review_helpful_button', function () {
+            var current = jQuery(this);
+
+            var data = {
+                prcProductId: prcProductId,
+                review_id: jQuery(this).data('review-id'),
+                helpfulness: jQuery(this).data('review-helpfulness')
+            };
+
+            jQuery.ajax({
+                type: "POST",
+                url: ajaxUrl + 'saveFeedback',
+                data: data,
+                cache: false,
+                success: function (data) {
+                    var json = jQuery.parseJSON(data);
+
+                    current.parent('.ekomi_review_helpful_question').hide();
+                    current.parent().prev('.ekomi_review_helpful_thankyou').show();
+                    current.parent().prev().prev('.ekomi_review_helpful_info').html(json.message);
+                }
+            });
+        });
+
+        // Loading reviews on paginatin
+        jQuery('body').on('click', '.loads_more_reviews', function (e) {
+            prcOffset = reviewsCountPage;
+
+            if (reviewsCountTotal / reviewsCountPage > 1) {
+                var data = {
+                    prcProductId: prcProductId,
+                    queryBy: queryBy,
+                    prcOffset: prcOffset,
+                    reviewsLimit: reviewsLimit,
+                    prcFilter: prcFilter
+                };
+
+                jQuery.ajax({
+                    type: "POST",
+                    url: ajaxUrl + 'loadReviews',
+                    data: data,
+                    cache: false,
+                    success: function (data) {
+                        var json = jQuery.parseJSON(data);
+
+                        reviewsCountPage = reviewsCountPage + json.count;
+                        jQuery('#ekomi_reviews_container').append(json.result);
+                        jQuery('.current_review_batch').text(reviewsCountPage);
+
+                        if (reviewsCountTotal / reviewsCountPage <= 1) {
+                            jQuery('.loads_more_reviews').hide();
+                        }
+                    }
+                });
+            } else {
+                jQuery('.loads_more_reviews').hide();
+            }
+        });
     });
-});
 }
